3e462ace3c53afb37bd85308d05da1e65466cb1d
wkleedorfer <wkleedorfer@6da68ee8-c7c7-11de-ba9a-9db95b2bc6c5>
2009-11-02 16:52:07 +0000

    Added comments to Problem 10

--- ProjectEuler/src/problem_10/Problem10.java
+++ ProjectEuler/src/problem_10/Problem10.java
@@ -6,15 +6,20 @@ import utils.PrimeGenerator;
 public class Problem10 {
 
 	public long getPrimeSum(long max) {
+		// init prime number generator
 		PrimeGenerator p = new PrimeGenerator();
 		long result = 0;
 		long prime;
 		
+		// get primes until conditions are met
 		while(true)
 		{
+			// get one prime after the other, let the implementation take care of the specifics
 			prime = p.getNextPrime();
+			// if we get a prime greater or equal to the specified maximum return with the result
 			if (prime >= max)
 				return result;
+			// add the prime to the result sum
 			result += prime;
 		}
 	}
