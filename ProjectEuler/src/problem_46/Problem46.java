ed3176bc1b2ff2300e7269823a9eb64623a96bfb
wkleedorfer <wkleedorfer@6da68ee8-c7c7-11de-ba9a-9db95b2bc6c5>
2010-08-09 13:16:12 +0000

    Problem 46 solved

--- ProjectEuler/src/problem_46/Problem46.java
+++ ProjectEuler/src/problem_46/Problem46.java
@@ -2,17 +2,53 @@ package problem_46;
 
 import utils.Primes;
 
+/**
+ * @author Wolfgang
+ * @note What is the smallest odd composite that cannot be written as the sum of a prime and twice a square? (Goldbach Conjecture)
+ */
 public class Problem46 {
 
-	public int getSolution()
+	/**
+	 * @return the smallest number that cannot be written as a sum of a prime and twice a square
+	 */
+	public long getSolution()
 	{
-		Primes _gen = new Primes(10000);
-		int result = 0;
-		for (int i = 35; i < Integer.MAX_VALUE; i += 2)
+		// Goldbach Conjecture formula: c = prime + 2 * n^2
+
+		// start with a small number of primes, the optimizer should add other primes when needed
+		Primes primes = new Primes(100);
+		// start with the number 35 -- see http://projecteuler.net/index.php?section=problems&id=46
+		long i = 35;
+		// start with the first prime though
+		long prime = 0;
+
+		// every uneven integer is a composite number
+		for (; i < Integer.MAX_VALUE; i += 2)
 		{
-			
+			// generate primes until primer is greater then the tested number itself
+			for (prime = 0; prime < i;)
+			{
+				// get next prime number greater than previous prime
+				try { prime = primes.generatePrime(prime); } catch (Exception e) {}
+				long j = 1;
+				long square = 1;
+				// now start calculating squares
+				do
+				{
+					square = j * j;
+					++j;
+				} while (prime + 2 * square < i);
+				
+				// check the goldbach forumula, if there is a number we can quit this test
+				if (prime + 2 * square == i)
+					break;
+			}
+			// if we did not find a prime (plus square) smaller than tested number this is our result
+			if (prime > i)
+				return i;
 		}
-		return 5777;
+		// we should never get here; ideally throw an exception
+		return 0;
 	}
 	
 }
