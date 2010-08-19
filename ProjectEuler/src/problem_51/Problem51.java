1e3e79b74c287b221c41f87a534c08d9d1a3459f
wkleedorfer <wkleedorfer@6da68ee8-c7c7-11de-ba9a-9db95b2bc6c5>
2010-08-19 13:39:33 +0000

    Problem 51 solved

---
+++ ProjectEuler/src/problem_51/Problem51.java
@@ -0,0 +1,157 @@
+package problem_51;
+
+import utils.ArrayList;
+import utils.Digits;
+import utils.Permutation;
+import utils.Primes;
+
+/**
+ * @author wolfgang
+ * @note Find the smallest prime which, by replacing part of the number (not necessarily adjacent digits) with the same digit, is part of an eight prime value family.
+ */
+public class Problem51 {
+	
+	final int ORDER = 8;
+	
+	/**
+	 * @param ordinal the length of the number the mask is for
+	 * @return the mask for the ordinal
+	 * @throws IndexOutOfBoundsException if ordinal doesn't match expectations
+	 */
+	public ArrayList<int[]> getMask(int ordinal) throws IndexOutOfBoundsException
+	{
+		// number must be at least 2 digit long
+		if (ordinal < 2)
+			throw new IndexOutOfBoundsException("Ordinal must at least be 2");
+		// decrease ordinal to match array index
+		ordinal -= 2;
+		
+		// create resulting array
+		ArrayList<int[]> result = new ArrayList<int[]>();
+		// create permutation object
+		Permutation perm = new Permutation();
+		// these are the base masks that will be permuted
+		int[] mask0 = new int[] {1000000,1100000,1110000,1111000,1111100,1111110};
+		int[] mask1 = new int[] {100000,110000,111000,111100,111110};
+		int[] mask2 = new int[] {10000,11000,11100,11110};
+		int[] mask3 = new int[] {1000,1100,1110};
+		int[] mask4 = new int[] {100,110};
+		int[] mask5 = new int[] {10};
+		// the combination of all masks for easy access with ordinal
+		int[][] mask = new int[][] {mask5, mask4, mask3, mask2, mask1, mask0};
+		
+		try
+		{
+			// now permute all masks for the given ordinal
+			for (int idx = 0; idx < mask[ordinal].length; ++idx)
+			{
+				ArrayList<int[]> tmp = perm.permutate(mask[ordinal][idx]);
+				// remove duplicate entries
+				tmp.distinct();
+				// add to resulting array
+				result.addAll(tmp);
+			}
+			return result;
+		} catch (Exception e)
+		{
+			throw new IndexOutOfBoundsException("Wrong ordinal" + ordinal + "(should be <= " + mask.length + ")");
+		}
+	}
+	
+	/**
+	 * @param test number to test
+	 * @param masks the mask for the number
+	 * @param primes the prime generator to check for primes
+	 * @param order how many generated primes are needed
+	 * @return the first prime >= <test> that generates <order> primes
+	 */
+	long applymask(long test, ArrayList<int[]> masks, Primes primes, int order)
+	{
+		// test never changes so convert to array immediately
+		int[] testa = Digits.Number((int)test);
+		int fail = 0;
+		int count = 0;
+		long firstprime = 0L;
+		long l = 0L;
+
+		// first iterate through all masks
+		for (int[] mask: masks)
+		{
+			// reinit the counters for each mask loop
+			fail = 0;
+			count = 0;
+			firstprime = 0;
+			
+			// do not use last digit since that would make the number non-prime
+			if (mask[mask.length -1] == 1)
+				continue;
+
+			// now iterate through all single digit numbers
+			for (int number = 0; number <= 9; ++number)
+			{
+				l = 0;
+				// looking at the result it seems that 100109 is not a valid result,
+				// which indicates that the most significant digit must not be zero
+				// so we skip all masks that have the most significant digit set and number equals zero
+				if (mask[0] == 1 && number == 0)
+					continue;
+				
+				// now generate the number by applying the mask to the test-array
+				for (int idx = mask.length - 1; idx >= 0; --idx)
+				{
+					if (mask[idx] == 0)
+						l += testa[idx] * Math.pow(10, mask.length-idx-1);
+					else
+						l += number * Math.pow(10, mask.length-idx-1);
+				}
+
+				// check the generated number for primeness
+				if (false == primes.isPrime(l))
+					++fail;	// fail counter to quit early
+				else
+				{
+					// this is to remember the first prime (it's possible that test is not the correct number (depending on the mask))
+					if (firstprime == 0L)
+						firstprime = l;
+					++count;
+				}
+				// check fail count, if there are more fails than would be allowed quit this mask
+				if (fail > (10 - order))
+					break;
+			}
+			// check the result against the wanted result
+			if (count >= order)
+				return firstprime;
+		}
+		return 0L;
+	}
+	
+	/**
+	 * @return the smallest prime that generates 8 primes
+	 * @throws Exception if no solution can be found
+	 */
+	public long getSolution() throws Exception {
+		// pregenerate the primes
+		Primes primes = new Primes(200000);
+		// mask object not yet created
+		ArrayList<int[]> mask = null;
+		
+		// there is probably a bigger start number that can be proven to work than 100
+		for (long test = 100; test < Long.MAX_VALUE;)
+		{
+			// get the next prime
+			try {test = primes.generatePrime(test);} catch(Exception ex) {break;}
+			// get the digits of the prime to get the correct mask
+			int log = (int) Math.log10(test) + 1;
+			// get the mask
+			mask = getMask(log);
+			// check the number
+			long result = applymask(test, mask, primes, ORDER);
+			// if the result is a number != 0 the algorithm returned something valid
+			if (result > 0) 
+				return result;
+		}
+		throw new Exception("No solution could be found");
+	}
+	
+}
---
+++ ProjectEuler/src/problem_51/Problem51Test.java
@@ -0,0 +1,74 @@
+package problem_51;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import utils.ArrayList;
+import utils.Primes;
+
+public class Problem51Test {
+
+		@Test
+		public void testMask()
+		{
+			// 100000,110000,111000,111100,111110,
+			//  10000,101000,110100,111010,111101,
+			//   1000,100100,110010,110110,111011,
+			//    100,100010,110001,101110,110111,
+			//     10,100001,101100, 11110,101111,
+			//      1, 11000,101010,111001, 11111,
+			//         10100,101001,110101,
+			//         10010,100110,101101,
+			//         10001,100101, 11101,
+			//          1100,100011,110011,
+			//          1010, 11100,101011,
+			//          1001, 11010, 11011,
+			//           110, 11001,100111,
+			//           101, 10110, 10111,
+			//            11, 10101,  1111,
+			//                10011,
+			//                 1110,
+			//                 1101,
+			//                 1011,
+			//                  111};
+			// 6	 +15    +20    +15     +6     = 62
+			Problem51 p = new Problem51();
+			assertEquals(62, p.getMask(6).size());
+			assertEquals(2, p.getMask(2).size());
+			try
+			{
+				p.getMask(1);
+				fail();
+			} catch (IndexOutOfBoundsException ex){}
+			try
+			{
+				p.getMask(9);
+				fail();
+			} catch (IndexOutOfBoundsException ex){}
+		}
+		
+		@Test
+		public void testMaskApplication()
+		{
+			Problem51 p = new Problem51();
+			Primes primes = new Primes(1000000);
+			ArrayList<int[]> tmp = p.getMask(2);
+			assertEquals(13, p.applymask(13, tmp, primes, 6));
+			tmp = p.getMask(5);
+			assertEquals(56003, p.applymask(56003, tmp, primes, 7));
+			tmp = p.getMask(6);
+			assertEquals(121313, p.applymask(121313, tmp, primes, 8));
+		}
+	
+		@Test
+		public void testSolution()
+		{
+			try
+			{
+			Problem51 p = new Problem51();
+			assertEquals(121313, p.getSolution());
+			} catch (Exception ex){ fail(); }
+		}
+}
